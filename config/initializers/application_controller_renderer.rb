feb4c27f1b7ecf68e913a992de296d3dd283749c
moyama55 <moyama04@gmail.com>
2021-10-17 07:30:19 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '312195ec6373653c90a672b291e909a36528a7b41e32e2e86afe5b40da6d20238eb7b0b852c85b64ef7ea4bca34f36537828bf8e33dc0c78fd571cb3ffeeadfa'
